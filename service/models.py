bb48248b316f11d16de92a1929ad081eeebd96d4
Maxwell <mxt.developer@gmail.com>
2021-05-09 18:08:06 +0200

    Refactor service models

--- service/models.py
+++ service/models.py
@@ -7,12 +7,12 @@ class Mapping(models.Model):
     updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 
 class MappingTitle(models.Model):
     title = models.CharField(max_length=256)
-    entries = models.ForeignKey(Mapping, on_delete=models.CASCADE)
+    mapping = models.ForeignKey(Mapping, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
