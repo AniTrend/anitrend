5247ffa3dc84988737bf284955dfff6f46dca0e4
Maxwell <mxt.developer@gmail.com>
2024-04-27 22:11:59 +0200

    refactor: update docker compose files and scripts

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Starting migrations"
-python manage.py migrate
+python manage.py makemigrations migrate
 
 echo 'Collecting static files...'
 python manage.py collectstatic --no-input
@@ -9,11 +9,15 @@ python manage.py collectstatic --no-input
 echo "Starting server"
 
 if [ "$1" = "--debug" ]; then
-  python manage.py runserver 0.0.0.0:8800
+  python manage.py runserver 0.0.0.0:8800 &
 else
   gunicorn "$APP_NAME.wsgi:application" \
     --bind "0.0.0.0:$GUNICORN_PORT" \
     --workers "$GUNICORN_WORKERS" \
     --timeout "$GUNICORN_TIMEOUT" \
-    --log-level "$GUNICORN_LOG_LEVEL"
+    --log-level "$GUNICORN_LOG_LEVEL" &
 fi
+
+
+echo "Starting qcluster"
+python manage.py qcluster &
