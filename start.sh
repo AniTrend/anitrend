789a67b0766c9385f2645e5efbbdeea3f4b9fd95
Maxwell Mapako <wax911@users.noreply.github.com>
2024-04-28 23:02:06 +0200

    chore: ensure that start.sh only runs cluster in background and server in foreground (#123)
    This would effectively prevent the container from shutting down prematurely when it has nothing is in the foreground

--- start.sh
+++ start.sh
@@ -6,18 +6,16 @@ python manage.py makemigrations migrate
 echo 'Collecting static files...'
 python manage.py collectstatic --no-input
 
-echo "Starting server"
+echo "Starting qcluster"
+python manage.py qcluster &
 
+echo "Starting server"
 if [ "$1" = "--debug" ]; then
-  python manage.py runserver 0.0.0.0:8800 &
+  python manage.py runserver 0.0.0.0:8800
 else
   gunicorn "$APP_NAME.wsgi:application" \
     --bind "0.0.0.0:$GUNICORN_PORT" \
     --workers "$GUNICORN_WORKERS" \
     --timeout "$GUNICORN_TIMEOUT" \
-    --log-level "$GUNICORN_LOG_LEVEL" &
+    --log-level "$GUNICORN_LOG_LEVEL"
 fi
-
-
-echo "Starting qcluster"
-python manage.py qcluster &
