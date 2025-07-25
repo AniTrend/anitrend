7fb0bc09bacac7031fbaa9a774e286dc087fda5c
Maxwell <mxt.developer@gmail.com>
2025-07-25 19:38:37 +0200

    feat: enhance OpenTelemetry integration with PostgreSQL instrumentation and utility functions

--- app/wsgi.py
+++ app/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from .otel_config import setup_otel
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings.production')
+
+# Initialize OpenTelemetry before creating the WSGI application
+APP_NAME = os.environ.get("OTEL_SERVICE_NAME", "anitrend")
+setup_otel(APP_NAME)
 
 application = get_wsgi_application()
